b1c8121e5b50b82f1e9a81fd8c2644dffa263f05
Nishant <getrooted0019@hotmail.com>
2021-06-20 19:18:16 +0530

    Changed theme.

--- src/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ src/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.diceroller
 
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
 
 class Dice {
 
@@ -17,15 +17,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get button object.
         val rollButton: Button = findViewById(R.id.button)
-        // Set onClick for out button.
         rollButton.setOnClickListener {
-            // Generate random number between 1 & 6.
             val diceVal = Dice().roll()
-            // Get textview object.
             val resultTextView: TextView = findViewById(R.id.textView)
-            // Set the random number as textview value.
             resultTextView.text = diceVal.toString()
         }
     }
