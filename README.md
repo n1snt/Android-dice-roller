85b5a57a516af4c3a5359d7de03ddcfb206eb96e
Nishant <getrooted0019@hotmail.com>
2021-06-22 12:07:30 +0530

    Replaced textView with ImageView.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Android-dice-roller
 An opensource minimal dice roller for Android.
+
+## Add screenshots here.
+
+## Add references here.
--- src/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ src/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class Dice {
-
     fun roll(): Int {
         return (1..6).random()
     }
@@ -20,8 +19,29 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             val diceVal = Dice().roll()
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = diceVal.toString()
+            val resultImageView: ImageView = findViewById(R.id.imageView)
+
+            when (diceVal) {
+                1 -> {
+                    resultImageView.setImageResource(R.drawable.dice_1)
+                }
+                2 -> {
+                    resultImageView.setImageResource(R.drawable.dice_2)
+                }
+                3 -> {
+                    resultImageView.setImageResource(R.drawable.dice_3)
+                }
+                4 -> {
+                    resultImageView.setImageResource(R.drawable.dice_4)
+                }
+                5 -> {
+                    resultImageView.setImageResource(R.drawable.dice_5)
+                }
+                6 -> {
+                    resultImageView.setImageResource(R.drawable.dice_6)
+                }
+            }
+
         }
     }
 }
