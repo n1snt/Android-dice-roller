428700681bfe2120e423a22568fa22ff5d61c103
Nishant <getrooted0019@hotmail.com>
2021-06-22 12:32:52 +0530

    Cleaned up code.

--- src/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ src/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,31 +5,51 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
-class Dice {
-    fun roll(): Int {
-        return (1..6).random()
-    }
-}
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Get button object.
         val rollButton: Button = findViewById(R.id.button)
+        // Set onclick for the button above.
         rollButton.setOnClickListener {
-            val diceVal = Dice().roll()
-            val resultImageView: ImageView = findViewById(R.id.imageView)
-
-            val drawableResource = when (diceVal) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
-            }
-            resultImageView.setImageResource(drawableResource)
+            rollDice()
         }
+        rollDice()
+    }
+
+    private fun rollDice() {
+        // Create dice object.
+        val diceVal = Dice().roll()
+
+        // Get image view.
+        val resultImageView: ImageView = findViewById(R.id.imageView)
+
+        // Check which image to set based on diceVal.
+        val drawableResource = when (diceVal) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Set the image view with the correct drawable.
+        resultImageView.setImageResource(drawableResource)
+
+        // Set content description for screen readers.
+        resultImageView.contentDescription = diceVal.toString()
     }
 }
+
+class Dice {
+    fun roll(): Int {
+        return (1..6).random()
+    }
+}
+
+
